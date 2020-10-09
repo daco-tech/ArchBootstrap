6e182cd1dfc246b04bf87631e0747cdd57305eec
Daniel <Costa>
2020-10-09 01:32:50 +0100

    Update setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -60,17 +60,110 @@ mount "${device}1" /mnt/boot/efi
 # Update Mirrors
 printinfo "Updating mirrors list"
 cp sysfiles/mirrorlist /etc/pacman.d/mirrorlist
-pacman -Syyu
 
-# Install Essential Packages
-printinfo "Running pacstrap on the root mount"
-pacstrap /mnt base linux linux-firmware --noconfirm
+
+# Config InitRamFS
+printinfo "Configuring initramfs"
+pacstrap -i /mnt mkinitcpio --noconfirm
+cp /mnt/etc/mkinitcpio.conf /mnt/etc/mkinitcpio.conf.backup
+
+
+# Configure Hardware specific
+_initramfs_modules="i915 rfkill"
+_initramfs_hooks="base autodetect udev keyboard keymap consolefont encrypt modconf block filesystems"
+sed -i -r "s/^MODULES=\(\)/MODULES=($_initramfs_modules)/" /mnt/etc/mkinitcpio.conf
+sed -i -r "s/^HOOKS=(.*)/HOOKS=($_initramfs_hooks)/" /mnt/etc/mkinitcpio.conf
+sed -i -r '/#COMPRESSION="lz4"/s/^#*//g' /mnt/etc/mkinitcpio.conf
+# The keymap needs to be configured earlier so initramfs uses the correct layout
+# for entering the disk decryption password.
+mkdir -p /mnt/usr/local/share/kbd/keymaps
+{ echo "keycode 58 = Escape";
+  echo "altgr keycode 18 = euro";
+  echo "altgr keycode 46 = cent"; } > /mnt/usr/local/share/kbd/keymaps/uncap.map
+{ echo "keycode 58 = Caps_Lock";
+  echo "altgr keycode 18 = euro";
+  echo "altgr keycode 46 = cent"; } > /mnt/usr/local/share/kbd/keymaps/recap.map
+{ echo "KEYMAP=pt-latin1";
+  echo "KEYMAP_TOGGLE=/usr/local/share/kbd/keymaps/uncap.map";
+  echo "FONT=ter-116n"; } > /mnt/etc/vconsole.conf
+
+
+
+# Install Packages
+printinfo "Installing Pacman packages"
+
+_pkgs_base=(base intel-ucode linux-lts linux-firmware)
+_pkgs_drivers=(intel-media-driver libva mesa ntfs-3g vulkan-intel
+               xf86-video-intel)
+_pkgs_sys=(avahi bash bluez coreutils dhcpcd efibootmgr exfat-utils f2fs-tools
+           fakeroot findutils fish fscrypt gptfdisk grub iwd lz4 pacman parted
+           patch pulseaudio rng-tools sudo xz zip zsh zstd)
+_pkgs_tools=(archiso aria2 bash-completion bat bc bluez-utils cbatticon cpupower croc curl
+             entr exa fd ffmpeg firejail fwupd fzf gawk gnupg gocryptfs htop
+             intel-gpu-tools intel-undervolt iotop libva-utils lshw lsof man
+             neovim nmap nnn openbsd-netcat openssh p7zip pbzip2 pigz powertop
+             progress ripgrep svt-av1 svt-hevc svt-vp9 tig time tmux tree unzip
+             usleep which)
+_pkgs_dev=(autoconf automake binaryen binutils bison clang cmake ctags diffutils
+           docker docker-compose edk2-ovmf gcc gcc8 gcc9 gdb git go go-tools
+           lldb m4 make ninja openssl-1.0 perf pkgconf python python-pip qemu
+           qemu-arch-extra spirv-llvm-translator spirv-headers spirv-tools
+           strace tokei vulkan-extra-layers vulkan-icd-loader vulkan-mesa-layers
+           vulkan-tools vulkan-validation-layers wabt zig)
+_pkgs_x11=(bspwm dunst picom sxhkd xorg-server xorg-xinit xorg-xinput xorg-xprop
+           xorg-xrandr xorg-xset xorg-xsetroot xsel)
+_pkgs_fonts=(noto-fonts noto-fonts-emoji terminus-font ttf-font-awesome
+             ttf-jetbrains-mono)
+_pkgs_apps=(alacritty arandr feh firefox libreoffice-still maim meld mesa-demos
+            mpv nomacs obs-studio pavucontrol pcmanfm-gtk3 rofi redshift
+            slock sxiv veracrypt wireshark-qt)
+
+pacman -Syy
+pacstrap -i /mnt ${_pkgs_base[*]} ${_pkgs_drivers[*]} ${_pkgs_sys[*]} \
+	${_pkgs_tools[*]} ${_pkgs_dev[*]}  ${_pkgs_x11[*]} ${_pkgs_fonts[*]} \
+	${_pkgs_apps[*]} --needed --noconfirm
+
+
+
+
+
+
+
+
+
+
 
 # Generate fstab
 printinfo "Generate fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
-#genfstab -U /mnt/efi >> /mnt/etc/fstab
+
+# Copy config files
+printinfo "Copy configuration files"
+
+cp sysfiles/resolv.conf /mnt/etc/resolv.conf
+
 
 # ChRoot
 printinfo "Change root to /mnt"
 arch-chroot /mnt
+
+
+###########################################################################################
+###########################################################################################
+################                    CONFIG USER SPACE                      ################
+###########################################################################################
+###########################################################################################
+
+# Quit Chroot and cleanup
+exit
+printinfo "Cleanup"
+umount /mnt/boot/efi
+umount /mnt/boot
+sync
+umount /mnt/proc/
+umount /mnt/sys/
+umount /mnt/dev/
+umount /mnt
+cryptsetup close root
+
+popd > /dev/null
