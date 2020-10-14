9b165f72baf04d650ebd4dc979647db76c242fad
Daniel <Costa>
2020-10-14 18:03:41 +0100

    Add System Settings X

--- base/4-install.sh
+++ base/4-install.sh
@@ -3,14 +3,13 @@
 printinfo "Installing System Packages"
 
 _pkgs_base=(base intel-ucode linux linux-firmware)
-_pkgs_drivers=(intel-media-driver libva mesa ntfs-3g vulkan-intel
-               xf86-video-intel)
+
 _pkgs_sys=(avahi bash bluez coreutils dhcpcd efibootmgr exfat-utils f2fs-tools
            fakeroot findutils fish fscrypt gptfdisk grub iwd lz4 pacman parted
            patch pulseaudio rng-tools sudo xz zip zsh zstd cups)
 _pkgs_tools=(archiso aria2 bash-completion bat bc bluez-utils cbatticon cpupower croc curl
              entr exa fd ffmpeg firejail fwupd fzf gawk gnupg gocryptfs htop
-             intel-gpu-tools intel-undervolt iotop libva-utils lshw lsof man
+             iotop libva-utils lshw lsof man
              neovim nmap nnn openbsd-netcat openssh p7zip pbzip2 pigz powertop
              progress ripgrep svt-av1 svt-hevc svt-vp9 tig time tmux tree unzip
              usleep which)
@@ -28,9 +27,9 @@ _pkgs_apps=(alacritty arandr feh firefox libreoffice-still maim meld mesa-demos
             mpv nomacs obs-studio pavucontrol pcmanfm-gtk3 rofi redshift
             slock sxiv veracrypt wireshark-qt)
 
-pacstrap -i /mnt ${_pkgs_base[*]} ${_pkgs_drivers[*]} ${_pkgs_sys[*]} --needed --noconfirm #\
-	#${_pkgs_tools[*]} ${_pkgs_dev[*]}  ${_pkgs_x11[*]} ${_pkgs_fonts[*]} \
-	#${_pkgs_apps[*]} ´
+pacstrap -i /mnt ${_pkgs_base[*]} ${_pkgs_drivers[*]} ${_pkgs_sys[*]} \
+	${_pkgs_tools[*]} ${_pkgs_dev[*]}  ${_pkgs_x11[*]} ${_pkgs_fonts[*]} \
+	${_pkgs_apps[*]} --needed --noconfirm
 
 printinfo "Generate fstab"
 
--- base/7.1-chroot.sh
+++ base/7.1-chroot.sh
@@ -61,3 +61,57 @@ systemctl enable fstrim.timer
 #systemctl enable intel-undervolt.service
 systemctl enable rngd.service
 #systemctl enable sshd.service
+
+
+
+
+
+
+cp /usr/share/doc/avahi/ssh.service /etc/avahi/services
+sed -i 's/hosts:.*/hosts: files mymachines myhostname mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] dns/' /etc/nsswitch.conf
+
+cp /tmp/chroot/sysfiles/bluetooth.conf /etc/bluetooth/main.conf
+cp /tmp/chroot/sysfiles/intel-undervolt.conf /etc/
+mkdir -p /etc/iwd && cp /tmp/chroot/sysfiles/iwd.conf /etc/iwd/main.conf
+cp /tmp/chroot/sysfiles/xorg.conf /etc/X11/xorg.conf.d/
+
+chmod u=rw,g=r,o=r /etc/bluetooth/main.conf /etc/intel-undervolt.conf \
+	/etc/iwd/main.conf /etc/X11/xorg.conf.d/xorg.conf
+
+cp /etc/makepkg.conf /etc/makepkg.conf.backup
+sed -i -r 's/^CFLAGS=".*"/CFLAGS="-march=native -O3 -pipe -fno-plt"/' /etc/makepkg.conf
+sed -i -r 's/^CXXFLAGS=".*"/CXXFLAGS="-march=native -O3 -pipe -fno-plt"/' /etc/makepkg.conf
+sed -i -r "s/^#MAKEFLAGS=\".*\"/MAKEFLAGS=\"-j$(nproc)\"/" /etc/makepkg.conf
+sed -i -r 's/^COMPRESSGZ=\(.*\)/COMPRESSGZ=(pigz -c -f -n)/' /etc/makepkg.conf
+sed -i -r 's/^COMPRESSBZ2=\(.*\)/COMPRESSBZ2=(pbzip2 -c -f)/' /etc/makepkg.conf
+sed -i -r 's/^COMPRESSXZ=\(.*\)/COMPRESSXZ=(xz -c -z --threads=0 -)/' /etc/makepkg.conf
+sed -i -r 's/^COMPRESSZST=\(.*\)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/' /etc/makepkg.conf
+
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+sed -i -r 's/#Port 22/Port 1337/' /etc/ssh/sshd_config
+sed -i -r '/#HostKey \/etc\/ssh\/ssh_host_ed25519_key/s/^#*//g' /etc/ssh/sshd_config
+sed -i -r '/#PasswordAuthentication yes/s/^#*//g' /etc/ssh/sshd_config
+sed -i -r '/#PermitEmptyPasswords no/s/^#*//g' /etc/ssh/sshd_config
+echo -e "\nAllowUsers\t${_user}" >> /etc/ssh/sshd_config
+
+cp /etc/sudoers /etc/sudoers.backup
+{ echo "Defaults timestamp_timeout=10";
+  echo "Defaults:%wheel targetpw";
+  echo -e "%wheel\tALL=(ALL) ALL";
+  echo "Cmnd_Alias CPUPWR = /usr/bin/cpupower";
+  echo "Cmnd_Alias IGPUFREQ = /usr/bin/intel_gpu_frequency";
+  echo "Cmnd_Alias IGPUTOP = /usr/bin/intel_gpu_top";
+  echo "Cmnd_Alias IOTOP = /usr/bin/iotop";
+  echo "Cmnd_Alias KBDR = /usr/bin/kbdrate";
+  echo "Cmnd_Alias MNT = /usr/bin/mount";
+  echo "Cmnd_Alias PS = /usr/bin/ps";
+  echo "Cmnd_Alias UMNT = /usr/bin/umount";
+  echo "root ALL=(ALL) ALL";
+  echo "${_user} ALL=(ALL) NOPASSWD: CPUPWR,IGPUFREQ,IGPUTOP,IOTOP,KBDR,MNT,PS,UMNT"; } \
+	> /etc/sudoers
+chown -c root:root /etc/sudoers
+chmod -c 0440 /etc/sudoers
+
+sed -i -r 's/#SystemMaxUse=/SystemMaxUse=256M/' /etc/systemd/journald.conf
+sed -i -r 's/#user_allow_other/user_allow_other/' /etc/fuse.conf
+echo "vm.vfs_cache_pressure=90" >> /etc/sysctl.d/99-swappiness.conf
