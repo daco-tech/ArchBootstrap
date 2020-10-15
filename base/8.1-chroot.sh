13fb6864294f7a05830321ab8aaf50111ed0e9ef
Daniel <Costa>
2020-10-15 21:03:48 +0100

    Fix

--- base/8.1-chroot.sh
+++ base/8.1-chroot.sh
@@ -4,26 +4,23 @@ source ../hosts/vbox.vars
 echo "Creating user accounts"
 
 
-useradd --create-home --groups users,wheel --shell "/usr/bin/zsh" "[USER]"
+useradd --create-home --groups users,wheel --shell "/usr/bin/zsh" "USER"
 passwd --delete root
-passwd --delete "[USER]"
+passwd --delete "USER"
 
-chown "[USER]:[USER]" "/home/[USER]"
+chown "USER:USER" "/home/USER"
 
-printinfo "Configuring user accounts"
+echo "Configuring user accounts"
 
-chown "[USER]:[USER]" "/tmp/chroot/users/[USER]/bootstrap.sh"
+chown "USER:USER" "/tmp/chroot/users/USER/bootstrap.sh"
 su -s /bin/bash -c \
-	"cd /tmp/chroot/ && . \"/tmp/chroot/users/[USER]/bootstrap.sh\" --host [HOST] --user [USER]" \
-	--login [USER]
+	"cd /tmp/chroot/ && . \"/tmp/chroot/users/USER/bootstrap.sh\" --host HOST --user USER" \
+	--login USER
 
-printinfo "\n"
-printinfo "+ ------------------ +"
-printinfo "| Accounts passwords |"
-printinfo "+ ------------------ +"
+echo "Accounts passwords"
 printwarn "Set root password!"
 passwd root
-printwarn "Set [USER] password!"
-passwd "[USER]"
+printwarn "Set USER password!"
+passwd "USER"
 
 popd > /dev/null
